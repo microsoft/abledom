32fe7193eaa27ddff409d221334bc4f80a2fc56c
Charles Assunção <charles.assuncao@microsoft.com>
2025-11-04 14:21:11 -0300

    feat: add valid html

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export { FocusLostRule } from "./rules/focuslost";
 export { BadFocusRule } from "./rules/badfocus";
 export { FindElementRule } from "./rules/find";
 export { CustomNotifyRule } from "./rules/notify";
+export { RequiredParentRule } from "./rules/requiredparent";
 export {
   isAccessibilityAffectingElement,
   hasAccessibilityAttribute,
