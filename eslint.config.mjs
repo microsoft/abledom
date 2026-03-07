c2091606cec693dae45c3a54559820da1d99833e
Tony Ganchev <tony.ganchev@gmail.com>
2026-03-07 09:15:03 +0200

    devops: switch to @tony.ganchev/eslint-plugin-header
    Reasons:
    - The fork is supported and updated.
    - ESLint 9-native with full schema support and needing no hacks.
    - Windows support
    - Multiple other bugfixes.
    - UX improvements - better error messages and more autofix capabilities.
    - Extensive documentation.
    - Better configuration format (although the old one is still supported).

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import js from "@eslint/js";
 import _import from "eslint-plugin-import";
-import header from "eslint-plugin-header";
+import header from "@tony.ganchev/eslint-plugin-header";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import globals from "globals";
-
-header.rules.header.meta.schema = false;
 
 export default [
   {
@@ -73,14 +71,20 @@ export default [
 
       "header/header": [
         1,
-        "block",
-        [
-          "!",
-          " * Copyright (c) Microsoft Corporation. All rights reserved.",
-          " * Licensed under the MIT License.",
-          " ",
-        ],
-        1,
+        {
+          header: {
+            commentType: "block",
+            lines: [
+              "!",
+              " * Copyright (c) Microsoft Corporation. All rights reserved.",
+              " * Licensed under the MIT License.",
+              " ",
+            ],
+          },
+          trailingEmptyLines: {
+            minimum: 1,
+          },
+        },
       ],
     },
   },
